c8d82f1f2553d7b591f7dfd7b50010d2f02077eb
Jamie Embleton <77155482+jamieemb@users.noreply.github.com>
2021-02-19 15:43:12 +0000

    Unit 1

---
+++ structs-and-algs/Unit 1/Task 1/src/DateOfBirthTest.java
@@ -0,0 +1,16 @@
+public class DateOfBirthTest {
+
+    public static void main (String[] args){
+
+        int day = Input.getInteger("What day were you born? >> ");
+        int month = Input.getInteger("What month were you born? >> ");
+        int year = Input.getInteger("What year were you born? >> ");
+
+        System.out.printf("Your DOB is: %d/%d/%d%n", day, month, year);
+    }
+
+
+
+
+
+}
